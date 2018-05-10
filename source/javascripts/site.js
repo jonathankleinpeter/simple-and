24da8bdea873119a5a9a275019b4386c08f0b2e9
Kleinpeter Jonathan <Jonathan@iMac-de-Jonathan-et-Chloe.local>
2018-05-11 00:24:07 +0200

    coucou

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -16,7 +16,7 @@ $( document ).ready(function() {
 	}
 	$(".presentation").html(text);
 
-	//animate word by word
+	//generate class calling
 
     var $span = [];
 
@@ -24,6 +24,29 @@ $( document ).ready(function() {
   		$span[i] = $(".word"+i);
   	}
 
- 	tl = new TimelineLite();
-    tl.staggerFrom($span, 0.8, {y: '+=40', autoAlpha: 0, ease:Power4.easeInOut}, 0.16);
+	//render animation
+
+  	function textSpliting() {
+ 		textSpliting = new TimelineLite();
+    	textSpliting.staggerFrom($span, animationTime*2, {y: '+=40', autoAlpha: 0, ease:Power4.easeInOut}, 0.16);
+    	return textSpliting;
+	}
+
+    //______________________MENU_ANIMATION_____________________//
+
+    function menuAnimation() {
+	    menuAnimation = new TimelineLite();
+		menuAnimation.from('.block-author', animationTime*2, {y: '-=100', autoAlpha: 0, ease:Power4.easeInOut});
+		menuAnimation.from('.block-menu', animationTime*2, {x: '-=40', autoAlpha: 0, ease:Power4.easeInOut}, 0);
+		return menuAnimation;
+	}
+
+	//______________________MASTER_ANIMATION_____________________//
+
+	//Manage all the animations
+	
+	master = new TimelineLite();
+
+	master.add(menuAnimation())
+	master.add(textSpliting(), '-=0.6');
 });
