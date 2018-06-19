a2ca4fe1e5aef1e0e22e835832497510fbc1dee7
Kleinpeter Jonathan <Jonathan@iMac-de-Jonathan-et-Chloe.local>
2018-06-20 00:21:40 +0200

    ok

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -2,144 +2,207 @@
 //= require "ScrollMagic"
 //= require "animation.gsap"
 //= require "debug.addIndicators"
+//= require "barba"
 
 $( document ).ready(function() {
 
-
-    // TEXT_SPLITING_ANIMATION
-    //========================================================
+    Barba.Pjax.start();
     var animationTime = 0.4;
-    var elementsToSplit = ".intro__presentation";
 
-    //Split span by words
+    Barba.Dispatcher.on('newPageReady', function() {
 
-    var arr = $(elementsToSplit).text().split(" ");
-    var text="";
-    for (i = 0; i < arr.length; ++i) {
-        text+= "<span class='words word" + i + "'>" + arr[i] + "&nbsp;" + "</span>";
-    }
-    $(".intro__presentation").html(text);
+        // TEXT_SPLITING_ANIMATION
+        //========================================================
 
-    //generate class calling
+        var elementsToSplit = ".intro__presentation";
 
-    var $span = [];
+        //Split span by words
 
-    for (i = 0; i <= arr.length; ++i) {
-        $span[i] = $(".word"+i);
-    }
-
-    function splitAnimation() {
-        splitAnimation = new TimelineLite();
-        splitAnimation.staggerFrom($span, animationTime*2, {y: '+=40', autoAlpha: 0, ease:Power4.easeInOut}, 0.16);
-        return splitAnimation;
-    }
-
-    // MENU_ANIMATION
-    //========================================================
-
-    function menuAnimation() {
-        menuAnimation = new TimelineLite();
-        menuAnimation.from('.logo', animationTime*1.5, {scaleX:0, scaleY:0, ease:Power4.easeInOut});
-        menuAnimation.from('.studio-name', animationTime*1.5, {y: '-=40', autoAlpha: 0, ease:Power4.easeInOut}, 0);
-        menuAnimation.from('.menu', animationTime*1.5, {x: '-=40', autoAlpha: 0, ease:Power4.easeInOut}, 0);
-        return menuAnimation;
-    }
-
-    // MASTER_ANIMATION
-    //========================================================
-
-    //Manage all the animations
-    
-    master = new TimelineLite();
-
-    master.add(splitAnimation(), '-=0.6');
-
-    // SUMMARY_ANIMATION
-    //========================================================
-
-    var $summaryPart1 = [$("#summary__work-cover1"), $("#summary__work-cover2"), $("#summary__work-cover3"), $("#summary__work-cover4"), $("#summary__work-cover5")];
-    var $summaryPart2 = [$("#summary__about-cover1"), $("#summary__about-cover2"), $("#summary__about-cover3"), $("#summary__about-cover4"), $("#summary__about-over5")];
-    var $summaryPart3 = [$("#summary__journal-cover1"), $("#summary__journal-cover2")];
-
-    // init
-    var ctrl = new ScrollMagic.Controller({
-    });
-
-    // Create scenes
-    $("#trigger1").each(function(i) {
-        var tl1 = new TimelineMax();
-        tl1.staggerTo($summaryPart1, animationTime, {y: -80, autoAlpha: 1, ease: Power4.easeOut}, animationTime/6);
-        new ScrollMagic.Scene({
-            triggerElement: this,
-        })
-        .setTween(tl1)
-        .addTo(ctrl);
-    });
-
-    $("#trigger2").each(function(i) {
-        var tl2 = new TimelineMax();
-        tl2.staggerTo($summaryPart2, animationTime, {y: -80, autoAlpha: 1, ease: Power4.easeOut}, animationTime/6);
-        new ScrollMagic.Scene({
-            triggerElement: this,
-        })
-        .setTween(tl2)
-        .addTo(ctrl);
-    });
-
-    $("#trigger3").each(function(i) {
-        var tl3 = new TimelineMax();
-        tl3.staggerTo($summaryPart3, animationTime, {y: -80, autoAlpha: 1, ease: Power4.easeOut}, animationTime/6);
-        new ScrollMagic.Scene({
-            triggerElement: this,
-        })
-        .setTween(tl3)
-        .addTo(ctrl);
-    });
-
-    // PICTURES_ANIMATION
-    //========================================================
-
-    // loop through all elements
-    $('.column__img').each(function() {
-        // build a tween
-        var tweenimg = TweenMax.from($(this), animationTime, {autoAlpha: 0, scale: 0.95, y: '+=40', ease: Power4.easeOut});
-
-        // build a scene
-        var sceneimg = new ScrollMagic.Scene({
-            triggerHook: 0.85,
-            triggerElement: this
-        })
-        .setTween(tweenimg) // trigger a TweenMax.to tween
-        .addTo(ctrl);      
-    });
-
-    // MENU_PAGE_STATE
-    //========================================================
-
-    var pageState = window.location.pathname
-    if (pageState == '/work.html') {
-        $('.menu__li').removeClass('menu__li--active')
-        $('.menu__li').eq(0).addClass('menu__li--active')
-    } else if (pageState == '/about.html') {
-        $('.menu__li').removeClass('menu__li--active')
-        $('.menu__li').eq(1).addClass('menu__li--active')
-    } else if (pageState == '/journal.html') {
-        $('.menu__li').removeClass('menu__li--active')
-        $('.menu__li').eq(2).addClass('menu__li--active')
-    }
-
-    // TOOLTIP
-    //========================================================
-
-    var tooltip = document.querySelectorAll('.titletip');
-    
-    document.addEventListener('mousemove', fn, false);
-    function fn(e) {
-        for (var i=tooltip.length; i--;) {
-            tooltip[i].style.left = e.clientX + 'px';
-            tooltip[i].style.top = e.clientY + 'px';
-            tooltip[i].style.transition = '0.2s';
+        var arr = $(elementsToSplit).text().split(" ");
+        var text="";
+        for (i = 0; i < arr.length; ++i) {
+            text+= "<span class='words word" + i + "'>" + arr[i] + "&nbsp;" + "</span>";
         }
-    }
+        $(".intro__presentation").html(text);
+
+        //generate class calling
+
+        var $span = [];
+
+        for (i = 0; i <= arr.length; ++i) {
+            $span[i] = $(".word"+i);
+        }
+
+        function splitAnimation() {
+            splitAnimation = new TimelineLite();
+            splitAnimation.staggerFrom($span, animationTime*2, {y: '+=40', autoAlpha: 0, ease:Power4.easeInOut}, 0.16);
+            return splitAnimation;
+        }
+
+        // MASTER_ANIMATION
+        //========================================================
+
+        master = new TimelineLite();
+        master.add(splitAnimation(), '-=0.6');
+        //Manage all the animations
+
+        // SUMMARY_ANIMATION
+        //========================================================
+
+        var $summaryPart1 = [$("#summary__work-cover1"), $("#summary__work-cover2"), $("#summary__work-cover3"), $("#summary__work-cover4"), $("#summary__work-cover5")];
+        var $summaryPart2 = [$("#summary__about-cover1"), $("#summary__about-cover2"), $("#summary__about-cover3"), $("#summary__about-cover4"), $("#summary__about-over5")];
+        var $summaryPart3 = [$("#summary__journal-cover1"), $("#summary__journal-cover2")];
+
+        // init
+        var ctrl = new ScrollMagic.Controller({
+        });
+
+        // Create scenes
+        $("#trigger1").each(function(i) {
+            var tl1 = new TimelineMax();
+            tl1.staggerTo($summaryPart1, animationTime, {y: -80, autoAlpha: 1, ease: Power4.easeOut}, animationTime/6);
+            new ScrollMagic.Scene({
+                triggerElement: this,
+            })
+            .setTween(tl1)
+            .addTo(ctrl);
+        });
+
+        $("#trigger2").each(function(i) {
+            var tl2 = new TimelineMax();
+            tl2.staggerTo($summaryPart2, animationTime, {y: -80, autoAlpha: 1, ease: Power4.easeOut}, animationTime/6);
+            new ScrollMagic.Scene({
+                triggerElement: this,
+            })
+            .setTween(tl2)
+            .addTo(ctrl);
+        });
+
+        $("#trigger3").each(function(i) {
+            var tl3 = new TimelineMax();
+            tl3.staggerTo($summaryPart3, animationTime, {y: -80, autoAlpha: 1, ease: Power4.easeOut}, animationTime/6);
+            new ScrollMagic.Scene({
+                triggerElement: this,
+            })
+            .setTween(tl3)
+            .addTo(ctrl);
+        });
+
+        // PICTURES_ANIMATION
+        //========================================================
+
+        // loop through all elements
+        $('.column__img').each(function() {
+            // build a tween
+            var tweenimg = TweenMax.from($(this), animationTime, {autoAlpha: 0, scale: 0.95, y: '+=40', ease: Power4.easeOut});
+
+            // build a scene
+            var sceneimg = new ScrollMagic.Scene({
+                triggerHook: 0.85,
+                triggerElement: this
+            })
+            .setTween(tweenimg) // trigger a TweenMax.to tween
+            .addTo(ctrl);      
+        });
+
+        // MENU_PAGE_STATE
+        //========================================================
+
+        var pageState = window.location.pathname
+        if (pageState == '/work.html') {
+            $('.menu__li').removeClass('menu__li--active')
+            $('.menu__li').eq(0).addClass('menu__li--active')
+        } else if (pageState == '/about.html') {
+            $('.menu__li').removeClass('menu__li--active')
+            $('.menu__li').eq(1).addClass('menu__li--active')
+        } else if (pageState == '/journal.html') {
+            $('.menu__li').removeClass('menu__li--active')
+            $('.menu__li').eq(2).addClass('menu__li--active')
+        }
+
+        // TOOLTIP
+        //========================================================
+
+        var tooltip = document.querySelectorAll('.titletip');
+        
+        document.addEventListener('mousemove', fn, false);
+        function fn(e) {
+            for (var i=tooltip.length; i--;) {
+                tooltip[i].style.left = e.clientX + 'px';
+                tooltip[i].style.top = e.clientY + 'px';
+                tooltip[i].style.transition = '0.2s';
+            }
+        }
+
+    });
+
+    // BARBA
+    //========================================================
+    Barba.Pjax.start();
+    var FadeTransition = Barba.BaseTransition.extend({
+        start: function() {
+            /**
+            * This function is automatically called as soon the Transition starts
+            * this.newContainerLoading is a Promise for the loading of the new container
+            * (Barba.js also comes with an handy Promise polyfill!)
+            */
+
+            // As soon the loading is finished and the old page is faded out, let's fade the new page
+            Promise
+            .all([this.newContainerLoading, this.fadeOut()])
+            .then(this.fadeIn.bind(this));
+        },
+
+        fadeOut: function() {
+            /**
+             * this.oldContainer is the HTMLElement of the old Container
+             */
+
+            return $(this.oldContainer).animate({ opacity: 0 }).promise();
+        },
+
+        fadeIn: function() {
+            /**
+            * this.newContainer is the HTMLElement of the new Container
+            * At this stage newContainer is on the DOM (inside our #barba-container and with visibility: hidden)
+            * Please note, newContainer is available just after newContainerLoading is resolved!
+            */
+
+            var _this = this;
+            var $el = $(this.newContainer);
+
+            $(this.oldContainer).hide();
+
+            $el.css({
+                visibility : 'visible',
+                opacity : 0
+            });
+
+            $el.animate({ opacity: 1 }, 400, function() {
+                /**
+                * Do not forget to call .done() as soon your transition is finished!
+                * .done() will automatically remove from the DOM the old Container
+                */
+                _this.done();
+            });
+        }
+    });
+
+    /**
+    * Next step, you have to tell Barba to use the new Transition
+    */
+
+    Barba.Pjax.getTransition = function() {
+        /**
+        * Here you can use your own logic!
+        * For example you can use different Transition based on the current page or link...
+        */
+
+        return FadeTransition;
+    };
+
+    Barba.Dispatcher.on(newPageReady, function() {
+      //your listener
+    });
 
 });
