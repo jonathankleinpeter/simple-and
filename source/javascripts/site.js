2083483537db5110e52badc3528f01f6eb780606
Kleinpeter Jonathan <Jonathan@iMac-de-Jonathan-et-Chloe.local>
2018-06-23 16:29:31 +0200

    ok

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -9,84 +9,121 @@ $( document ).ready(function() {
     Barba.Pjax.start();
     var animationTime = 0.4;
 
+    // This is the js associate with a specific page. This will be loaded by Barba.js when the user trying to load the associated page
+
+    var Homepage = Barba.BaseView.extend({
+        namespace: 'homepage',
+        onEnter: function() {
+
+            // TEXT_SPLITING_ANIMATION
+            //========================================================
+
+            var elementsToSplit = ".intro__presentation";
+
+            //Split span by words
+
+            var arr = $(elementsToSplit).text().split(" ");
+            var text="";
+            for (i = 0; i < arr.length; ++i) {
+                text+= "<span class='words word" + i + "'>" + arr[i] + "&nbsp;" + "</span>";
+            }
+            $(".intro__presentation").html(text);
+
+            //generate class calling
+
+            var $span = [];
+
+            for (i = 0; i <= arr.length; ++i) {
+                $span[i] = $(".word"+i);
+            }
+
+            function splitAnimation() {
+                splitAnimation = new TimelineLite();
+                splitAnimation.staggerFrom($span, animationTime*2, {y: '+=40', autoAlpha: 0, ease:Power4.easeInOut}, 0.16);
+                return splitAnimation;
+            }
+
+            // MASTER_ANIMATION
+            //========================================================
+
+            master = new TimelineLite();
+            master.add(splitAnimation(), '-=0.6');
+            //Manage all the animations
+
+            // SUMMARY_ANIMATION
+            //========================================================
+
+            var $summaryPart1 = [$("#summary__work-cover1"), $("#summary__work-cover2"), $("#summary__work-cover3"), $("#summary__work-cover4"), $("#summary__work-cover5")];
+            var $summaryPart2 = [$("#summary__about-cover1"), $("#summary__about-cover2"), $("#summary__about-cover3"), $("#summary__about-cover4"), $("#summary__about-over5")];
+            var $summaryPart3 = [$("#summary__journal-cover1"), $("#summary__journal-cover2")];
+
+            // init
+            var ctrl = new ScrollMagic.Controller({
+            });
+
+            // Create scenes
+            $("#trigger1").each(function(i) {
+                var tl1 = new TimelineMax();
+                tl1.staggerTo($summaryPart1, animationTime, {y: -80, autoAlpha: 1, ease: Power4.easeOut}, animationTime/6);
+                new ScrollMagic.Scene({
+                    triggerElement: this,
+                })
+                .setTween(tl1)
+                .addTo(ctrl);
+            });
+
+            $("#trigger2").each(function(i) {
+                var tl2 = new TimelineMax();
+                tl2.staggerTo($summaryPart2, animationTime, {y: -80, autoAlpha: 1, ease: Power4.easeOut}, animationTime/6);
+                new ScrollMagic.Scene({
+                    triggerElement: this,
+                })
+                .setTween(tl2)
+                .addTo(ctrl);
+            });
+
+            $("#trigger3").each(function(i) {
+                var tl3 = new TimelineMax();
+                tl3.staggerTo($summaryPart3, animationTime, {y: -80, autoAlpha: 1, ease: Power4.easeOut}, animationTime/6);
+                new ScrollMagic.Scene({
+                    triggerElement: this,
+                })
+                .setTween(tl3)
+                .addTo(ctrl);
+            });
+
+            // REINITIATE PAGE_STATE
+            //========================================================
+            $('.menu__li').removeClass('menu__li--active')
+        }
+    });
+
+    var Work = Barba.BaseView.extend({
+        namespace: 'work',
+        onEnter: function() {
+
+            // TOOLTIP
+            //========================================================
+
+            var tooltip = document.querySelectorAll('.titletip');
+            
+            document.addEventListener('mousemove', fn, false);
+            function fn(e) {
+                for (var i=tooltip.length; i--;) {
+                    tooltip[i].style.left = e.clientX + 'px';
+                    tooltip[i].style.top = e.clientY + 'px';
+                    tooltip[i].style.transition = '0.2s';
+                }
+            }
+        }
+    });
+
+    // Init the specific js pages
+
+    Homepage.init();
+    Work.init();
+
     Barba.Dispatcher.on('newPageReady', function() {
-
-        // TEXT_SPLITING_ANIMATION
-        //========================================================
-
-        var elementsToSplit = ".intro__presentation";
-
-        //Split span by words
-
-        var arr = $(elementsToSplit).text().split(" ");
-        var text="";
-        for (i = 0; i < arr.length; ++i) {
-            text+= "<span class='words word" + i + "'>" + arr[i] + "&nbsp;" + "</span>";
-        }
-        $(".intro__presentation").html(text);
-
-        //generate class calling
-
-        var $span = [];
-
-        for (i = 0; i <= arr.length; ++i) {
-            $span[i] = $(".word"+i);
-        }
-
-        function splitAnimation() {
-            splitAnimation = new TimelineLite();
-            splitAnimation.staggerFrom($span, animationTime*2, {y: '+=40', autoAlpha: 0, ease:Power4.easeInOut}, 0.16);
-            return splitAnimation;
-        }
-
-        // MASTER_ANIMATION
-        //========================================================
-
-        master = new TimelineLite();
-        master.add(splitAnimation(), '-=0.6');
-        //Manage all the animations
-
-        // SUMMARY_ANIMATION
-        //========================================================
-
-        var $summaryPart1 = [$("#summary__work-cover1"), $("#summary__work-cover2"), $("#summary__work-cover3"), $("#summary__work-cover4"), $("#summary__work-cover5")];
-        var $summaryPart2 = [$("#summary__about-cover1"), $("#summary__about-cover2"), $("#summary__about-cover3"), $("#summary__about-cover4"), $("#summary__about-over5")];
-        var $summaryPart3 = [$("#summary__journal-cover1"), $("#summary__journal-cover2")];
-
-        // init
-        var ctrl = new ScrollMagic.Controller({
-        });
-
-        // Create scenes
-        $("#trigger1").each(function(i) {
-            var tl1 = new TimelineMax();
-            tl1.staggerTo($summaryPart1, animationTime, {y: -80, autoAlpha: 1, ease: Power4.easeOut}, animationTime/6);
-            new ScrollMagic.Scene({
-                triggerElement: this,
-            })
-            .setTween(tl1)
-            .addTo(ctrl);
-        });
-
-        $("#trigger2").each(function(i) {
-            var tl2 = new TimelineMax();
-            tl2.staggerTo($summaryPart2, animationTime, {y: -80, autoAlpha: 1, ease: Power4.easeOut}, animationTime/6);
-            new ScrollMagic.Scene({
-                triggerElement: this,
-            })
-            .setTween(tl2)
-            .addTo(ctrl);
-        });
-
-        $("#trigger3").each(function(i) {
-            var tl3 = new TimelineMax();
-            tl3.staggerTo($summaryPart3, animationTime, {y: -80, autoAlpha: 1, ease: Power4.easeOut}, animationTime/6);
-            new ScrollMagic.Scene({
-                triggerElement: this,
-            })
-            .setTween(tl3)
-            .addTo(ctrl);
-        });
 
         // PICTURES_ANIMATION
         //========================================================
@@ -119,25 +156,11 @@ $( document ).ready(function() {
             $('.menu__li').removeClass('menu__li--active')
             $('.menu__li').eq(2).addClass('menu__li--active')
         }
-
-        // TOOLTIP
-        //========================================================
-
-        var tooltip = document.querySelectorAll('.titletip');
-        
-        document.addEventListener('mousemove', fn, false);
-        function fn(e) {
-            for (var i=tooltip.length; i--;) {
-                tooltip[i].style.left = e.clientX + 'px';
-                tooltip[i].style.top = e.clientY + 'px';
-                tooltip[i].style.transition = '0.2s';
-            }
-        }
-
     });
 
-    // BARBA
+    // BARBA BASIC PAGE TRANSITION
     //========================================================
+
     Barba.Pjax.start();
     var FadeTransition = Barba.BaseTransition.extend({
         start: function() {
@@ -185,6 +208,7 @@ $( document ).ready(function() {
                 */
                 _this.done();
             });
+            document.body.scrollTop = 0;
         }
     });
 
@@ -200,9 +224,5 @@ $( document ).ready(function() {
 
         return FadeTransition;
     };
-
-    Barba.Dispatcher.on(newPageReady, function() {
-      //your listener
-    });
 
 });
