9c32712888a0a63b7eef57baa6d38150ce3859e8
chloeheinis <chloeheinis@MacBook-Pro-de-Chloe-HEINIS.local>
2018-07-28 10:09:25 +0200

    coucou jojo

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -159,6 +159,33 @@ $( document ).ready(function() {
         }
     });
 
+    // HEAD_RELOAD
+    //========================================================
+
+    Barba.Dispatcher.on('newPageReady', function(currentStatus, oldStatus, container, newPageRawHTML) {
+        // html head parser borrowed from jquery pjax
+        var $newPageHead = $( '<head />' ).html(
+            $.parseHTML(
+                newPageRawHTML.match(/<head[^>]*>([\s\S.]*)<\/head>/i)[0]
+                , document
+                , true
+            )
+        );
+        var headTags = [
+            //"meta[name='keywords']",
+            "meta[name='description']",
+            "meta[property^='og']",
+            //"meta[name^='twitter']",
+            //"meta[itemprop]",
+            //"link[itemprop]",
+            //"link[rel='prev']",
+            //"link[rel='next']",
+            //"link[rel='canonical']"
+        ].join(',');
+        $( 'head' ).find( headTags ).remove(); // Remove current head tags
+        $newPageHead.find( headTags ).appendTo( 'head' ); // Append new tags to the head
+    });
+
     // BARBA BASIC PAGE TRANSITION
     //========================================================
 
